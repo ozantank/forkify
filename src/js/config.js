1cbbb7203b720fd021e723e1e74703c477652a42
ozantank <ozantank21@gmail.com>
2023-09-21 22:24:16 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '4e2239ff-7fff-4926-b4c5-29976f6ea865';
+
+export const MODAL_CLOSE_SEC = 2.5;
