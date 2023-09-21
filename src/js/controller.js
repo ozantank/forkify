ddd86127b80f90e854012e1aaaf99347fcdf94b4
ozantank <ozantank21@gmail.com>
2023-09-21 22:33:36 +0300

    new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,5 +133,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+
+  console.log('');
 };
 init();
